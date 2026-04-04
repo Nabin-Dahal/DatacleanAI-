31ab520ef2ac1dd58b21e2efff9531a5ce18f43f
Nabin-Dahal <nabindahal741014@gmail.com>
2026-04-04 14:39:27 +0545

    database setup

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+import {createCilent} from '@supabase/supabase-js';
+
+const supabaseUrl =  process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createCilent(supabaseUrl, supabaseAnonKey); 
